52bee554d51c6d1886ec6f4e2cd5244e9f0be06d
raidas <mrraidas@gmail.com>
2023-02-21 20:05:59 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -35,16 +35,18 @@ onMounted(() => {
       </div>
     </div>
     <div style="position: absolute; bottom: 10%; left: 0; right: 0">
-      <a href="https://github.com/RaidasGrisk" target="_blank">github</a><br>
-      <a href="https://raidasgrisk.github.io/" target="_blank">stories</a><br>
-      <n-tooltip placement="bottom" trigger="click" :show-arrow="false">
-        <template #trigger>
-          <a @click="copyToClipboard()" style="cursor: pointer;">
-            email
-          </a>
-        </template>
-        <span> Copied! </span>
-      </n-tooltip>
+      <div :style="isMobile ? 'font-size: 1.3em;' : ''">
+        <a href="https://github.com/RaidasGrisk" target="_blank">github</a><br>
+        <a href="https://raidasgrisk.github.io/" target="_blank">stories</a><br>
+        <n-tooltip placement="bottom" trigger="click" :show-arrow="false">
+          <template #trigger>
+            <a @click="copyToClipboard()" style="cursor: pointer;">
+              email
+            </a>
+          </template>
+          <span> Copied! </span>
+        </n-tooltip>
+      </div>
     </div>
   </div>
 </template>
